ac6107df7d6aecffd90e5c26aed7f37285dcbef5
Player5987 <gaurish.malhotra.ug24@nsut.ac.in>
2024-08-24 01:56:32 +0530

    Add files via upload

---
+++ Identify the personal details of student.py
@@ -0,0 +1,23 @@
+# Identify the personal details of students from your school identity card and write a program to accept thes
+
+name_of_school = input("Enter tne Name of School: ")
+stu_name = input("Enter the Name of Student: ")
+roll = int(input("Enter the Roll number: "))
+clas = input("Enter the Class: ")
+address = input("Enter the Address: ")
+city = input("Enter the City name: ")
+pin = int(input("Enter the Pin Code: "))
+phone = int(input("Enter the Parent Phone number: "))
+print ()
+print ("-"*60)
+print ("|             ", name_of_school)
+print ("|")
+print ("| Student Name: ", stu_name, "    Roll NO: ", roll)
+print ("| Class: ", clas)
+print ("| Address: ", address)
+print ("|")
+print ("| City: ", city, "    Pin Code: ", pin)
+print ("| Prent's/ Guardian's Contact No: ", phone)
+print ("-"*60)
+
+
